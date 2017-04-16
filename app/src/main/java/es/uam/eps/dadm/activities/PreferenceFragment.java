b459003892b47653ba3d8e18dd2a91b1edf124b3
Pablo Pérez <92manso@gmail.com>
2017-04-16 19:15:08 +0200

    Javadoc PreferenceFragment
    + Comentado todo el código de PreferenceFragment para el javadoc

--- app/src/main/java/es/uam/eps/dadm/activities/PreferenceFragment.java
+++ app/src/main/java/es/uam/eps/dadm/activities/PreferenceFragment.java
@@ -3,7 +3,19 @@ package es.uam.eps.dadm.activities;
 import android.os.Bundle;
 import es.uam.eps.dadm.R;
 
+/**
+ * PreferenceFragment muestra las preferencias modificables de la app
+ *
+ * @author Pablo Manso
+ * @version 12/04/2017
+ */
 public class PreferenceFragment extends android.preference.PreferenceFragment {
+
+    /**
+     * Crea todo lo necesario para la correcta ejecución del fragmento
+     * @param savedInstanceState Pares clave valor que nos pasa la actividad que nos invoca
+     */
+    @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
