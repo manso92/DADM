e861619c03e7282a28b1b8a7c49090c891d66059
Pablo Pérez <92manso@gmail.com>
2017-04-16 18:05:14 +0200

    Javadoc PreferenceActivity
    + Generado el javadoc de PreferenceActivity

--- app/src/main/java/es/uam/eps/dadm/activities/PreferenceActivity.java
+++ app/src/main/java/es/uam/eps/dadm/activities/PreferenceActivity.java
@@ -11,63 +11,125 @@ import android.support.v7.app.AppCompatActivity;
 
 import es.uam.eps.dadm.R;
 
+/**
+ * PreferenceActivity muestra una actividad desde la que modificar todas las preferencias de usuario.
+ * Asímismo provee de las funciones necesarias para modificar y obtener las diferentes claves
+ * que se van a guardar en la app.
+ *
+ * @author Pablo Manso
+ * @version 12/04/2017
+ */
 public class PreferenceActivity extends AppCompatActivity {
-    private final static String BOARDSIZE_KEY = "boardsize";
-    public final static String BOARDSIZE_DEFAULT = "8";
+    /**
+     * Clave para registrar el UUID del usuario
+     */
     private final static String PLAYERUUID_KEY = "playeruuid";
-    public final static String PLAYERUUID_DEFAULT = "";
+
+    /**
+     * UUID por defecto para el usuario
+     */
+    public final static String PLAYERUUID_DEFAULT = "280f83bc-cc34-4744-b20c-b70f0765c846";
+
+    /**
+     * Clave para registrar el nombre de usuario
+     */
     private final static String PLAYERNAME_KEY = "playername";
+
+    /**
+     * Nombre por defecto para el usuario
+     */
     public final static String PLAYERNAME_DEFAULT = "Player";
-    private final static String PLAYERPASSWORD_KEY = "playerpassword";
-    public final static String PLAYERPASSWORD_DEFAULT = "Password";
+
+    /**
+     * Crea todo lo necesario para la correcta ejecución de la actividad
+     * @param savedInstanceState Pares clave valor que nos pasa la actividad que nos invoca
+     */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Colocamos el layout del fragment
         setContentView(R.layout.activity_fragment);
+        // Creamos un fragment manager para colocar el PreferenceFragment
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         PreferenceFragment fragment = new PreferenceFragment();
+        // Cambiamos el contenido por defecto pro nuestro fragmento
         fragmentTransaction.replace(android.R.id.content, fragment);
         fragmentTransaction.commit();
     }
+
+    /**
+     * Limpia las preferencias de usuario
+     * @param context Contexto desde el cual se quieren modificar las preferencias
+     */
     public static void resetPreferences(Context context){
+        // Obtenemos una referencia de las preferencias de usuario
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        // Creamos una referencia del editor de claves
         SharedPreferences.Editor editor = sharedPreferences.edit();
+        // Limpia las preferencias y commiteamos el cambio
         editor.clear();
         editor.commit();
     }
-    public static int getBoardSize(Context context) {
-        return Integer.parseInt(getKey(context, BOARDSIZE_KEY, BOARDSIZE_DEFAULT));
-    }
-    public static void setBoardsize(Context context, int size) {
-        setKey(context, PreferenceActivity.BOARDSIZE_KEY, Integer.toString(size));
-    }
+
+    /**
+     * Devuelve el valor del identificador de usuario que está registrado en las preferencias de usuario
+     * @param context Contexto desde el cual se quiere obtener el valor del usuario
+     * @return Identificador de usuario registrado | Default value para el identificador de usuario
+     */
     public static String getPlayerUUID(Context context) {
         return getKey(context, PLAYERUUID_KEY, PLAYERUUID_DEFAULT);
     }
+
+    /**
+     * Registra el identificador del usuario logueado en las preferencias de usuario
+     * @param context Contexto desde el cual se quiere registrar el valor
+     * @param uuid Identificador de usuario
+     */
     public static void setPlayerUUID(Context context, String uuid) {
         setKey(context, PreferenceActivity.PLAYERUUID_KEY, uuid);
     }
+
+    /**
+     * Devuelve el valor del nombre de usuario que está registrado en las preferencias de usuario
+     * @param context Contexto desde el cual se quiere obtener el valor del usuario
+     * @return Nombre de usuario registrado | Default value para el nombre de usuario
+     */
     public static String getPlayerName(Context context) {
         return getKey(context, PLAYERNAME_KEY, PLAYERNAME_DEFAULT);
     }
+
+    /**
+     * Registra el nombre del usuario logueado en las preferencias de usuario
+     * @param context Contexto desde el cual se quiere registrar el valor
+     * @param name Nombre de usuario
+     */
     public static void setPlayerName(Context context, String name) {
         setKey(context, PreferenceActivity.PLAYERNAME_KEY, name);
     }
-    public static String getPlayerPassword(Context context) {
-        return getKey(context, PLAYERPASSWORD_KEY, PLAYERPASSWORD_DEFAULT);
-    }
 
-    public static void setPlayerPassword(Context context, String password) {
-        setKey(context, PreferenceActivity.PLAYERPASSWORD_KEY, password);
-    }
-
+    /**
+     * Registra una clave en las preferencias de usuario
+     * @param context Contexto desde el cual se quiere registrar el valor
+     * @param key Clave que se quiere registrar
+     * @param value Valor que se le quiere dar a la clave
+     */
     private static void setKey(Context context, String key, String value){
+        // Obtenemos una referencia de las preferencias de usuario
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        // Creamos una referencia del editor de claves
         SharedPreferences.Editor editor = sharedPreferences.edit();
+        // Registramos la clave y commiteamos el cambio
         editor.putString(key, value);
         editor.commit();
     }
 
+    /**
+     * Obtiene una clave de las preferencias de usuario
+     * @param context Contexto desde el cual se pide el valor
+     * @param key Clave que se está pidiendo
+     * @param defaultValue Valor por defecto en caso de no encontrarse la clave
+     * @return String asociado a la clave que se ha provisto
+     */
     private static String getKey(Context context, String key, String defaultValue){
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(key, defaultValue);
