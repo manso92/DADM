3fc3a9982f0fb8f1b1f79035ea006dc00a930f78
Pablo Pérez <92manso@gmail.com>
2017-05-02 23:34:45 +0200

    Repositorio a elección
    + Ahora a parte de coger el repositorio de las preferencias se puede pedir uno de los repositorios manualmente

--- app/src/main/java/es/uam/eps/dadm/model/RoundRepositoryFactory.java
+++ app/src/main/java/es/uam/eps/dadm/model/RoundRepositoryFactory.java
@@ -1,9 +1,12 @@
 package es.uam.eps.dadm.model;
+
 import android.content.Context;
 import android.support.annotation.Nullable;
 
-import es.uam.eps.dadm.activities.PreferenceActivity;
 import es.uam.eps.dadm.database.DataBase;
+import es.uam.eps.dadm.server.ServerRepository;
+import es.uam.eps.dadm.view.activities.PreferenceActivity;
+
 
 /**
  * Clase manejadora de el almacenamiento de datos, con la cual crearemos de forma estática una
@@ -23,7 +26,34 @@ public class RoundRepositoryFactory {
     @Nullable
     public static RoundRepository createRepository(Context context) {
         // Creamos una referencia de la base de datos, local o en remoto
-        RoundRepository repository = PreferenceActivity.getOnlineGame(context) ? new DataBase(context) : new DataBase(context);
+        RoundRepository repository = PreferenceActivity.getOnlineGame(context) ?
+                ServerRepository.getInstance(context) :
+                new DataBase(context);
+        try {
+            // Abrimos la referencia en el gestor de datos
+            repository.open();
+        }
+        catch (Exception e) {
+            // Si hay alguna excepción al abrirlo, devolvemos null
+            return null;
+        }
+        // Devolvemos la referencia a la base de datos
+        return repository;
+    }
+
+    /**
+     * Crea una instancia del almacenamiento de datos y lo devuelve para poder manejar toda la
+     * lógica de la app
+     * @param context Contexto desde el cual se invoca el almacenamiento
+     * @param remoto Nos indica si el respositorio es local o remoto
+     * @return Referencia al almacenamiento de datos
+     */
+    @Nullable
+    public static RoundRepository createRepository(Context context, boolean remoto) {
+        // Creamos una referencia de la base de datos, local o en remoto
+        RoundRepository repository = remoto ?
+                ServerRepository.getInstance(context) :
+                new DataBase(context);
         try {
             // Abrimos la referencia en el gestor de datos
             repository.open();
