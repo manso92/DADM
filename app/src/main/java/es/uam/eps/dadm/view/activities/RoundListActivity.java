1eb02b9f3f243d4a7f7049436d955b20fc6cfa1f
Pablo Pérez <92manso@gmail.com>
2017-05-09 18:40:52 +0200

    Corregida creación de fragments
    * Ahroa para invocar los listfragments de partidas no hay que pasar el repositorio, se encargan ellos de hacerlo

--- app/src/main/java/es/uam/eps/dadm/view/activities/RoundListActivity.java
+++ app/src/main/java/es/uam/eps/dadm/view/activities/RoundListActivity.java
@@ -85,14 +85,14 @@ public class RoundListActivity extends AppCompatActivity implements RoundListFra
     private void setupViewPager(ViewPager viewPager) {
         // Creamos un adapter que contendrá nuestros fragmentos
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        RoundRepository localRepo = RoundRepositoryFactory.createRepository(this,false);
-        RoundRepository serverRepo = RoundRepositoryFactory.createRepository(this,true);
+
         // Añadimos los fragmentos que vamos a tener en nuestro adapter
-        adapter.addFragment(RoundListFragment.newInstance(localRepo, Round.Type.LOCAL), "Local");
-        adapter.addFragment(RoundListFragment.newInstance(serverRepo, Round.Type.ACTIVE), "Mis partidas");
-        adapter.addFragment(RoundListFragment.newInstance(serverRepo, Round.Type.OPEN), "Partidas abiertas");
-        adapter.addFragment(RoundListFragment.newInstance(serverRepo, Round.Type.FINISHED), "Finalizadas");
+        adapter.addFragment(RoundListFragment.newInstance(Round.Type.LOCAL), "Local");
+        adapter.addFragment(RoundListFragment.newInstance(Round.Type.ACTIVE), "Mis partidas");
+        adapter.addFragment(RoundListFragment.newInstance(Round.Type.OPEN), "Partidas abiertas");
+        adapter.addFragment(RoundListFragment.newInstance(Round.Type.FINISHED), "Finalizadas");
         adapter.addFragment(new BlankFragment(), "Estadísticas");
+
         // Vinculamos el adapter al viewpager
         viewPager.setAdapter(adapter);
     }
