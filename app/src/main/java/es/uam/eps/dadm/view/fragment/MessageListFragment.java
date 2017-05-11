841ec9b07faa416e7658c2d7e4a69b3fbaa63ff8
Pablo Pérez <92manso@gmail.com>
2017-05-12 00:49:49 +0200

    Fragment List Chats
    + Fragment que muestra la lista de chats que se tienen disponibles

---
+++ app/src/main/java/es/uam/eps/dadm/view/fragment/MessageListFragment.java
@@ -0,0 +1,190 @@
+package es.uam.eps.dadm.view.fragment;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import es.uam.eps.dadm.R;
+import es.uam.eps.dadm.events.GreenRobotEventBus;
+import es.uam.eps.dadm.events.NewMessageEvent;
+import es.uam.eps.dadm.model.Preferences;
+import es.uam.eps.dadm.model.RoundRepository;
+import es.uam.eps.dadm.model.RoundRepositoryFactory;
+import es.uam.eps.dadm.server.ServerRepository;
+import es.uam.eps.dadm.view.activities.Jarvis;
+import es.uam.eps.dadm.view.adapters.ChatsAdapter;
+import es.uam.eps.dadm.view.adapters.Message;
+import es.uam.eps.dadm.view.adapters.MessagesAdapter;
+import es.uam.eps.multij.ExcepcionJuego;
+
+/**
+ * RoundFragment es un fragmento que mostrará una lista de mensajes
+ *
+ * @author Pablo Manso
+ * @version 11/05/2017
+ */
+public class MessageListFragment extends Fragment {
+
+    /**
+     * Tag para escribir en el log
+     */
+    public static final String DEBUG = "Damas.MessageFrag";
+
+    /**
+     * Recyclerview que contendrá la lista de chats
+     */
+    @BindView(R.id.recycler_view)
+    RecyclerView chatRecyclerView;
+
+    /**
+     * Swipe to reload view para recargar la lista
+     */
+    @BindView(R.id.swiperefresh)
+    SwipeRefreshLayout swiperefresh;
+
+    /**
+     * Fab para añadir un nuevo chat
+     */
+    @BindView(R.id.fab)
+    FloatingActionButton addFoundFab;
+
+
+    /**
+     * Constante que nos representará de parámetro en el bundle
+     */
+    private static final String ARG_SENDER_ID = "sender";
+
+    /**
+     * Adaptador donde colocaremos todas nuestros mensajes
+     */
+    private ChatsAdapter adapter;
+
+    /**
+     * Instancia necesaria para hacer unbinding de los componentes
+     */
+    private Unbinder unbinder;
+
+    /**
+     * Repositorio del servidor del que obtendremos y enviaremos los mensajes
+     */
+    private ServerRepository serverRepository;
+
+    /**
+     * Prepara el fragmento para su ejecución
+     * @param savedInstanceState Pares clave-valor con los datos que serán necesarios para la ejecución del fragmento
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Creamos la instancia del repositorio
+        this.serverRepository = (ServerRepository) RoundRepositoryFactory.createRepository(this.getContext(), true);
+    }
+
+    /**
+     * Ejecutará las acciones necesarias para cuando se cree la vista
+     * @param inflater Clase que se encargará de mostrar los elementos del fragment
+     * @param container Contenedor del fragment
+     * @param savedInstanceState Pares clave valor que se nos dan como parámetro
+     * @return View que se acaba de crear
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Cargamos el layout del fragmento y hacemos binding de las vistas
+        final View view = inflater.inflate(R.layout.fragment_recycler, container, false);
+        this.unbinder = ButterKnife.bind(this, view);
+
+        // Cogemos el layout manager y le ajustamos un linearlayout para la lista
+        this.chatRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        this.chatRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        return view;
+    }
+
+    /**
+     * Ejecución al inicio del fragmento
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Empezamos a capturar los eventos
+        GreenRobotEventBus.getInstance().register(this);
+
+        // Actualizamos la lista de mensajes
+        this.updateUI();
+    }
+
+    /**
+     * Función que se ejecutará cuando se destruya la view
+     */
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Hacemos unbinding de todas las vistas que Butterknife utilizara antes
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // Dejamos de campturar eventos
+        GreenRobotEventBus.getInstance().unregister(this);
+    }
+
+    /**
+     * Actualiza la lista de mensajes en el recyclerview
+     */
+    public void updateUI() {
+        // Si está cargado el roundadapter, lo vaciamos
+        if(this.adapter != null) this.adapter.clear();
+
+        // Registramos el callback que manejará la lista de partidas devueltas
+        ServerRepository.MessagesCallback messagesCallback = new ServerRepository.MessagesCallback() {
+            @Override
+            public void onResponse(List<Message> messages) {
+                // Si no hay adapter lo creamos, y si lo hay, las añadimos
+                if (adapter == null)
+                    adapter = new ChatsAdapter(messages,getContext());
+                else
+                    adapter.addMessages(messages);
+
+                // Añadimos el adapter al recyclerview
+                if (chatRecyclerView != null)
+                    chatRecyclerView.setAdapter(adapter);
+
+                // Hacemos scrollhasta la última posición
+                chatRecyclerView.scrollToPosition(messages.size()-1);
+
+            }
+            @Override
+            public void onError(String error) {
+                // Mostramos el error que nos indican al llamar al callback
+                Snackbar.make(chatRecyclerView, error, Snackbar.LENGTH_LONG).show();
+            }
+        };
+        // Regcargamos la lista de rondas disponibles
+        serverRepository.getLastMessages(messagesCallback);
+    }
+}
+
