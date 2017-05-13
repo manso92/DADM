bd19c668ed582e013685566ac43adf0c3ceb82c4
Pablo Pérez <92manso@gmail.com>
2017-05-13 18:56:36 +0200

    Actualizado el evento de mensajes
    + Se ha añadido la modificación en la que se puede indicar la duración del mensaje  y la posibilidad de mostrar directamente el aviso

--- app/src/main/java/es/uam/eps/dadm/events/ShowMsgEvent.java
+++ app/src/main/java/es/uam/eps/dadm/events/ShowMsgEvent.java
@@ -1,5 +1,9 @@
 package es.uam.eps.dadm.events;
 
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Toast;
+
 /**
  * Clase que se encargará de indicar el error que hay que mostrar por pantalla
  *
@@ -22,14 +26,46 @@ public class ShowMsgEvent {
      * Mesnaje de error a mostrar
      */
     private String msg;
-    
+
+    /**
+     * Duración del mensaje
+     */
+    private int duracion;
+
     public ShowMsgEvent(Type tipo, String msg) {
         this.tipo = tipo;
         this.msg = msg;
+        switch (tipo) {
+            case SNACKBAR:
+                this.duracion = Snackbar.LENGTH_LONG;
+                break;
+            case TOAST:
+                this.duracion = Toast.LENGTH_LONG;
+                break;
+        }
+    }
+
+    public ShowMsgEvent(Type tipo, String msg, int duracion) {
+        this.tipo = tipo;
+        this.msg = msg;
+        this.duracion = duracion;
+    }
+
+    public void show(View view) {
+        switch (this.getTipo()) {
+            case SNACKBAR:
+                Snackbar.make(view, this.getMsg(), this.getDuracion()).show();
+                break;
+            case TOAST:
+                Toast.makeText(view.getContext().getApplicationContext(), this.getMsg(), Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
     public Type getTipo() { return tipo; }
     public void setTipo(Type tipo) { this.tipo = tipo; }
     public String getMsg() { return msg; }
     public void setMsg(String msg) { this.msg = msg; }
+    public int getDuracion() { return duracion; }
+    public void setDuracion(int duracion) { this.duracion = duracion; }
 }
